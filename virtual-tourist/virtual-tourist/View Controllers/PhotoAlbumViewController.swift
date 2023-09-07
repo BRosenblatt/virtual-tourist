a8353348b69e80b30262f300e176e23929a40ad1
Rebecca Rosenblatt <reprose@gmail.com>
2023-09-07 13:52:41 -0400

    Create view controllers for map and photo album

---
+++ virtual-tourist/virtual-tourist/View Controllers/PhotoAlbumViewController.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoAlbumViewController.swift
+//  virtual-tourist
+//
+//  Created by Matt Kauper on 9/7/23.
+//
+
+import UIKit
+
+class PhotoAlbumViewController: UIViewController {
+    
+}
--- virtual-tourist/virtual-tourist/View Controllers/TravelLocationsMapViewController.swift
+++ virtual-tourist/virtual-tourist/View Controllers/TravelLocationsMapViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TravelLocationsMapViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
